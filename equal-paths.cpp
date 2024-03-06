d14df019b9417e34d956af93419c4a247288dbfb
Gianna Besmonte <gbesmont@usc.edu>
2024-03-06 07:22:52 +0000

    finished equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -13,6 +13,49 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+	if (root == nullptr){
+		return true;
+	}
 
+	int _lHeight = treeHeight(root->left);
+	int _rHeight = treeHeight(root->right);
+
+	if (root->left != nullptr && root->right != nullptr && _lHeight - _rHeight != 0){
+		return false;
+	}
+
+	bool _lEqual = 1;
+	if (root->left){
+		_lEqual = equalPaths(root->left);
+	}
+	bool _rEqual = 1;
+	if (root->right){
+		_rEqual = equalPaths(root->right);
+	}
+
+	if (_lEqual && _rEqual){
+		return true;
+	}
+
+	return false;
 }
 
+int treeHeight(Node* root){
+	if (root == nullptr){
+		return 0;
+	}
+
+	if (root->left == nullptr && root->right == nullptr){
+		return 1;
+	}
+
+	int _left = treeHeight(root->left);
+	int _right = treeHeight(root->right);
+	
+	if (_left >= _right){
+		return _left + 1;
+	}
+	else{
+		return _right + 1;
+	}
+}
